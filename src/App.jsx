f24812ec6db307cce5f7005e98e58aa1b6048924
Prajil <Prajil7425@gmail.com>
2023-06-17 13:30:04 +0530

    Add skeleton files for components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import { fetchDataFromApi } from "./utils/apiUtils";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import Home from "./pages/home/Home";
+import PageNotFound from "./pages/pageNotFound/PageNotFound";
+import SearchResult from "./pages/searchResult/SearchResult";
 
 function App() {
   useEffect(() => {
